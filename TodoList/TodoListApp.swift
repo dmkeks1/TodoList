460a5ff1ae79a4c583bc19bcf4ac34e436c4da61
Dominik Zehe <zehedominik@googlemail.com>
2024-11-05 20:54:31 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Dominik Zehe on 05.11.24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
